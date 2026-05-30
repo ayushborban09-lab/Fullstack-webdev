906cd7e444495cf8849edcbb5201c0a933d1ba2e
Ayush borban <ayushborban09@gmail.com>
2026-05-30 16:11:03 +0530

    JavaScript day5 project

---
+++ javascriptt/day5/script.js
@@ -0,0 +1,25 @@
+let inp = document.querySelector("input");
+let add= document.querySelector("#add");
+let tasks = document.querySelector(".tasks")
+let edit =document.querySelector("#edit")
+let del =document.querySelector("#del")
+add.addEventListener("click",()=>{
+    let value = inp.value
+ 
+    if(!value) return
+
+    tasks.innerHTML +=`<div class="li">
+        <h2>${value}</h2>
+        <div>
+          <button id="edit">Edit</button>
+          <button id="del">Delete</button>
+        </div>
+      </div>`
+
+      inp.value=""
+     
+
+
+
+})
+
