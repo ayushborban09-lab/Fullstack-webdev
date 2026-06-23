183be455e64972a1850e81f8a3e7aed606e52c4a
Ayush borban <ayushborban09@gmail.com>
2026-06-23 18:51:13 +0530

    Update: add react00 project and modify .gitignore

---
+++ react00/script.js
@@ -0,0 +1,89 @@
+// console.log("hello");
+
+// let a = 40;
+
+// let sum = (num1, num2) => {
+//   let response = num1 + num2;
+//   return response;
+// };
+
+// let c = sum(46, 91);
+
+// let fn = (...args) => {
+//   console.log(args);
+//   //   return a;
+// };
+
+// let res = fn(4, 6, 7, 8, 9);
+
+// console.log(res);
+
+// destructuring...
+
+// let { age, address } = obj;
+
+// console.log(age, address);
+
+// let p = {
+//   id: 1,
+//   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//   price: 109.95,
+//   description:
+//     "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//   category: "men's clothing",
+//   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//   rating: {
+//     rate: 3.9,
+//     count: 120,
+//   },
+// };
+
+// let {
+//   rating: { rate },
+//   id,
+//   price,
+// } = p;
+
+// console.log(rate, id, price);
+
+// let arr = [
+//   "zeel",
+//   "toi",
+//   {
+//     name: "rahul",
+//   },
+// ];
+
+// let arr = [56, 90];
+
+// let [point, score] = arr;
+
+// console.log(score, point);
+
+let fn = ({ name, age }) => {
+  console.log(name, age);
+};
+
+fn({
+  name: "Raghav",
+  age: 89,
+  address: "Saket nagar",
+  company: "Google",
+});
+
+// let obj = {
+//   name: "Raghav",
+//   age: 89,
+//   address: "Saket nagar",
+//   company: "Google",
+// };
+
+// console.log(obj);
+
+let arr1 = [2, 4, 5, 6];
+
+let arr2 = ["hello", "polo", "taigun"];
+
+let newArr = arr1;
+
+console.log(newArr);
