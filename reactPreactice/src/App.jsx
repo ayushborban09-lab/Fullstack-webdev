c5fb624e9d46061d46b5921c1a640fde94a1bc33
Ayush borban <ayushborban09@gmail.com>
2026-07-02 01:05:21 +0530

    Add reactPreactice project and fix App CSS layout

---
+++ reactPreactice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Manu from './Manu'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Manu/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ reactPreactice/src/Manu.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+const Manu = () => {
+  return (
+    <main>
+      <nav>
+      <div className="navLeft">
+        <img
+          width="80px"
+          src="https://uhf.microsoft.com/images/microsoft/RE1Mu3b.png"
+          alt=""
+        />
+        <div className="navLinks">
+          <a href="">Microsoft365</a>
+          <a href="">Teams</a>
+          <a href="">Copilots</a>
+          <a href="">Windows</a>
+          <a href="">Surface</a>
+          <a href="">Xbox</a>
+          <a href="">Support</a>
+        </div>
+      </div>
+      <div className="navRight">
+        <a href="">All microsoft</a>
+        <i className="ri-search-line"></i>
+        <i className="ri-shopping-cart-line"></i>
+        <div className="profile"></div>
+      </div>
+    </nav>
+    <div className="main1">
+      <h2>Meet Surface Laptop</h2>
+      <p>
+        Unlock AI features like Live Captions and Cocreator with this
+        exceptionally powerful laptop.
+      </p>
+      <button>Learn more</button>
+    </div>
+
+    <div className="main2">
+      <div className="main2Top">
+        <div className="center">
+          <div className="centerBox">
+            <i className="ri-microsoft-fill"></i>
+            <p>Choose your Microsoft 365</p>
+          </div>
+          <div className="centerBox">
+            <i className="ri-xbox-fill"></i>
+            <p>Shop Xbox</p>
+          </div>
+          <div className="centerBox">
+            <i className="ri-microsoft-fill"></i>
+            <p>Get Windows 11</p>
+          </div>
+          <div className="centerBox">
+            <i className="ri-tablet-line"></i>
+            <p>Explore Surface Device</p>
+          </div>
+        </div>
+      </div>
+      <div className="main2Bottom">
+
+        <div className="bottomBoxes">
+          <div className="boxTop"></div>
+          <div className="boxContent">
+            <h2>Achieve the extraordinary</h2>
+            <p>
+              Microsoft 365 delivers cloud storage, security and Microsoft
+              Copilot in your favourite apps – all in one plan.
+            </p>
+            <button>Shop Microsoft 365</button>
+          </div>
+        </div>
+
+        <div className="bottomBoxes">
+          <div className="boxTop"></div>
+          <div className="boxContent">
+            <h2>Achieve the extraordinary</h2>
+            <p>
+              Microsoft 365 delivers cloud storage, security and Microsoft
+              Copilot in your favourite apps – all in one plan.
+            </p>
+            <button>Shop Microsoft 365</button>
+          </div>
+        </div>
+
+        <div className="bottomBoxes">
+          <div className="boxTop"></div>
+          <div className="boxContent">
+            <h2>Achieve the extraordinary</h2>
+            <p>
+              Microsoft 365 delivers cloud storage, security and Microsoft
+              Copilot in your favourite apps – all in one plan.
+            </p>
+            <button>Shop Microsoft 365</button>
+          </div>
+        </div>
+        <div className="bottomBoxes">
+          <div className="boxTop"></div>
+          <div className="boxContent">
+            <h2>Achieve the extraordinary</h2>
+            <p>
+              Microsoft 365 delivers cloud storage, security and Microsoft
+              Copilot in your favourite apps – all in one plan.
+            </p>
+            <button>Shop Microsoft 365</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </main>
+  )
+}
+
+export default Manu
