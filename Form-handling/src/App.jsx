9d0bc8d83b2f4c0bed1ee4d11715ae35b381bdb3
Ayush borban <ayushborban09@gmail.com>
2026-07-11 02:29:14 +0530

    chore: commit current workspace changes for push

---
+++ Form-handling/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+      <h1>Form handling</h1>
+      <form style={
+        
+      }>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="mobile" />
+        <input type="password" placeholder="Password" />
+        
+      </form>
+    </div>
+  )
+}
+
+export default App
--- props drilling/src/App.jsx
+++ props drilling/src/App.jsx
@@ -1,5 +1,55 @@
 import React from 'react'
 
+
+ let arr =[
+  {
+    name:ayuuuuuuuuuu,
+    age:25,
+    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  },
+  
+  {
+    name:ayuuuuuuuuuu,
+    age:25,
+    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  },
+  
+  {
+    name:ayuuuuuuuuuu,
+    age:25,
+    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  },
+  
+  {
+    name:ayuuuuuuuuuu,
+    age:25,
+    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  },
+  {
+    name:ayuuuuuuuuuu,
+    age:25,
+    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  },
+
+
+  
+ ]
+
+
+
+//   return (
+//     <div className="gap-5 h-screen p-4 flex">
+//       {arr.map((elem) => {
+//         return <UserCard user={elem} />;
+//       })}
+//     </div>
+//   );
+// };
 const App = () => {
   
   return (
