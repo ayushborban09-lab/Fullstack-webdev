9271f638345b27d174a2a638fcf1ea1a2d727053
Ayush borban <ayushborban09@gmail.com>
2026-07-11 23:50:12 +0530

    chore: push latest workspace changes

--- props-drilling/src/App.jsx
+++ props-drilling/src/App.jsx
@@ -1,67 +1,43 @@
 import React from 'react'
-import Card1 from './card1'
+import Card1 from './Card1'
 
-
- let arr =[
+const arr = [
   {
-    name:ayuuuuuuuuuu,
-    age:25,
-    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
-  },
-  
-  {
-    name:ayuuuuuuuuuu,
-    age:25,
-    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
-  },
-  
-  {
-    name:ayuuuuuuuuuu,
-    age:25,
-    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
-  },
-  
-  {
-    name:ayuuuuuuuuuu,
-    age:25,
-    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
+    name: 'ayuuuuuuuuuu',
+    age: 25,
+    image: 'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
-    name:ayuuuuuuuuuu,
-    age:25,
-    Image:'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
+    name: 'ayuuuuuuuuuu',
+    age: 25,
+    image: 'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
+  {
+    name: 'ayuuuuuuuuuu',
+    age: 25,
+    image: 'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'ayuuuuuuuuuu',
+    age: 25,
+    image: 'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'ayuuuuuuuuuu',
+    age: 25,
+    image: 'https://images.unsplash.com/photo-1783321284120-61d9a47c92a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+]
 
-
-  
- ]
-
-
-
-//   return (
-//     <div className="gap-5 h-screen p-4 flex">
-//       {arr.map((elem) => {
-//         return <UserCard user={elem} />;
-//       })}
-//     </div>
-//   );
-// };
 const App = () => {
-  
   return (
-    <div>
-      {
-        arr.map((elem) => {
-          return <Card1 user={elem} />;
-        })
-      }
+    <div className="gap-5 h-screen p-4 flex">
+      {arr.map((elem, index) => {
+        return <Card1 key={index} user={elem} />
+      })}
     </div>
   )
 }
 
-export default App  
+export default App
+
--- props-drilling/src/Card1.jsx
+++ props-drilling/src/Card1.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-useState
-const Comp3 = () => {
+import React from 'react'
+
+const Card1 = ({ user }) => {
   return (
-    <div className ="w-50 border h-fit p-4 flex flex col gap-">
-      <div>img</div>
-      <div>
-        <h1>Name-{name }</h1>
-        <p>Age-{age }</p>
-
+    <div className="w-50 border h-fit p-4 flex flex-col gap-4 rounded border-gray-500">
+      <div className="h-50 w-40">
+        <img className="h-full w-full object-cover" src={user.image} alt={user.name} />
       </div>
-      <button>Delete</button>
-
+      <div>
+        <h1>Name - {user.name}</h1>
+        <p>Age - {user.age}</p>
+      </div>
+      <button className="p-2 bg-red-600 text-white">Delete</button>
     </div>
   )
 }
 
-export default Comp3
+export default Card1
