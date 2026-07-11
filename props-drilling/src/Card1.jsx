af59ea769302f4767e2a992f6e0e4152d23f8342
Ayush borban <ayushborban09@gmail.com>
2026-07-11 23:12:11 +0530

    chore: push workspace changes

--- props-drilling/src/Card1.jsx
+++ props-drilling/src/Card1.jsx
@@ -2,8 +2,15 @@ import React, { useState } from 'react'
 useState
 const Comp3 = () => {
   return (
-    <div>
-      
+    <div className ="w-50 border h-fit p-4 flex flex col gap-">
+      <div>img</div>
+      <div>
+        <h1>Name-{name }</h1>
+        <p>Age-{age }</p>
+
+      </div>
+      <button>Delete</button>
+
     </div>
   )
 }
