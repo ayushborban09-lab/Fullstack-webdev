7fdb528f72eefae8c6f86e44032f193b4d3851b0
Ayush borban <ayushborban09@gmail.com>
2026-07-14 01:38:24 +0530

    Update Form-handling project

--- Form-handling/src/App.jsx
+++ Form-handling/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen gap-4 bg-red-200 ">
 
-      <h1>Form handling</h1>
-      <form style={
-        
-      }>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="mobile" />
-        <input type="password" placeholder="Password" />
+      <h1 className ="text-5xl font-bold" >Form handling</h1>
+      <form  className=" flex flex-col gap-6 bg-gray-200 p-4 rounded-lg shadow-lg h-70 w-90">
+        <input 
+        onchange={(e)=>console.log(e.target.value)}
+         className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" placeholder="Name" />
+        <input className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" placeholder="Email" />
+        <input className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" placeholder="mobile" />
+        <input className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" type="password" placeholder="Password" />
         
       </form>
     </div>
