d6a4824ab1584bbd28b06a5ea2d40275486d11d8
Ayush borban <ayushborban09@gmail.com>
2026-07-14 17:50:23 +0530

    Update Form-handling components

--- Form-handling/src/App.jsx
+++ Form-handling/src/App.jsx
@@ -2,18 +2,13 @@ import React, { useState } from 'react'
 import Cardd from "./compnent/cardd";
 
 const App = () => {
-  const [formValues, setFormValues] = useState({
-    name: "",
-    email: "",
-    mobile: "",
-    password: "",
-  });
-  const [user, setUser] = useState([]);
+  const [formValues, setFormValues] = useState({  });
+  const [users, setUsers] = useState([]);
 
 
-  const formSubmit = (e) => {                 // form submit handler
+  const formSubmit = (e) => {                 //ider formSubmit ka function bana diya kyuki hum form ko submit karna chahte hai or inche call kr rha hai 
     e.preventDefault();
-    setUser((prev) => [...prev, formValues]);
+    setUsers([...users, formValues]);
     setFormValues({
       name: "",
       email: "",
@@ -35,35 +30,36 @@ const App = () => {
       <h1 className="text-5xl font-bold" >Form handling</h1>
       <form className=" flex flex-col gap-6 bg-gray-200 p-4 rounded-lg shadow-lg h-70 w-90">
         <input
-          onchange={(e) => setFormValues({ ...formValues, name: e.target.value })}
+          onChange={(e) => setFormValues({ ...formValues, name: e.target.value })}
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text" placeholder="Name" />
 
 
-        <input onchange={(e) => setFormValues({ ...formValues, email: e.target.value })}
+        <input onChange={(e) => setFormValues({ ...formValues, email: e.target.value })}
 
 
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="email" placeholder="Email" />
 
         <input
-          onchange={(e) => setFormValues({ ...formValues, mobile: e.target.value })}
+          onChange={(e) => setFormValues({ ...formValues, mobile: e.target.value })}
 
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text" placeholder="mobile" />
 
 
         <input
-          onchange={(e) => setFormValues({ ...formValues, password: e.target.value })}
+          onChange={(e) => setFormValues({ ...formValues, password: e.target.value })}
 
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="password" placeholder="Password" />
+          
         <button className="bg-blue-500 text-white m-2 py-2 px-4 rounded-md hover:bg-blue-600">
           Submit</button>
       </form>
 
-      <div>
-        {user.map((elem) => {
+      <div className="flex flex-col items-center justify-center h-70 gap-4  ">
+        {users.map((elem) => {
           return <Cardd value={elem} />
 
         })}
--- Form-handling/src/compnent/cardd.jsx
+++ Form-handling/src/compnent/cardd.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const cardd = ({value}) => {
   return (
     <div
-    className="flex flex-col items-center justify-center h-screen gap-4 bg-red-200 ">
+    className="flex flex-col items-center justify-center h-70 gap-4 bg-pink-200 font-bold p-4 rounded-lg shadow-lg w-90">
       <h1>Name - {value.name}</h1>
       <h2>Email - {value.email}</h2>
       <h3>mobile - {value.mobile}</h3>
